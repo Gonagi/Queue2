a1519a2d4fe63f260fbc4d7b13798ca6d48f8874
Gonagi <kkh10002@naver.com>
2022-04-19 17:46:49 +0900

    최종

--- Queue2/main.c
+++ Queue2/main.c
@@ -1,1 +1,37 @@
 #include "queue2.h"
+
+int main()
+{
+	Queue q1 = Create_queue();
+	Queue q2 = Create_queue();
+	Queue q3 = Create_queue();
+
+	Enqueue(q1, 1);
+	Enqueue(q2, 10);
+	Enqueue(q3, 100);
+	Enqueue(q3, 200);
+
+	Print_queue(q1);
+	Print_queue(q2);
+	Print_queue(q3);
+
+	printf("q1 Peek : %d\n", Peek(q1));
+	printf("q1 Dequeue : %d\n", Dequeue(q1));
+	printf("q2 Dequeue : %d\n", Dequeue(q2));
+
+	printf("\n\n");
+
+	q1 = Create_queue();
+	q2 = Create_queue();
+
+	Enqueue(q2, 20);
+	Enqueue(q2, 30);
+	Enqueue(q2, 40);
+	Enqueue(q3, 300);
+
+	Print_queue(q1);
+	Print_queue(q2);
+	Print_queue(q3);
+	_CrtDumpMemoryLeaks();
+	return 0;
+}
